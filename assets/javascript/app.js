04ae088b2fdcc3e5d461341e88722f04b537ebc7
Jacob Sipple <sipple3@gmail.com>
2019-01-30 14:04:24 -0500

    takes user off active if they close the browser

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -12,12 +12,143 @@ let database = firebase.database();
 // change this to firebase as that's where this should be pulled from
 let username;
 let message;
+let user1 = undefined;
+let user2 = undefined;
+let user1Losses = 0
+let user2Losses = 0
+let user2Ties = 0
+let user2Wins = 0
+let user1Ties = 0
+let user1Wins = 0
+let rock1 = $("<button>").text("rock").addClass("rock1").on("click", function() {
+  database.ref().child("choice1").set({
+    choice: "rock"
+  })
+})
+let paper1 = $("<button>").text("paper").addClass("paper1").on("click", function() {
+  database.ref().child("choice1").set({
+    choice: "paper"
+  })
+})
+let sissors1 = $("<button>").text("sissors").addClass("sissor1").on("click", function() {
+  database.ref().child("choice1").set({
+    choice: "scissor"
+  })
+})
+let rock2 = $("<button>").text("rock").addClass("rock2").on("click", function() {
+  database.ref().child("choice2").set({
+    choice: "rock"
+  })
+})
+let paper2 = $("<button>").text("paper").addClass("paper2").on("click", function() {
+  database.ref().child("choice2").set({
+    choice: "paper"
+  })
+})
+let sissors2 = $("<button>").text("sissors").addClass("sissor2").on("click", function() {
+  database.ref().child("choice2").set({
+    choice: "scissor"
+  })
+})
+// database.ref().child("player1").on("child_added", function(snapshot) {
+//   user1=snapshot.val().username
+//   console.log(snapshot.val().username)
+// })
+// database.ref().child("player2").on("child_added", function(snapshot) {
+//   user2=snapshot.val().username
+// })
+
+database.ref().child("choice2").on("value", function(snapshot) {
+  choice2 = snapshot.val().choice
+  database.ref().child("choice1").on("value", function(snapshot) {
+    choice1 = snapshot.val().choice
+  })
+  if (choice1 !== null && choice2 !== null) {
+  if (choice1 === "rock") {
+    if (choice2 === "rock") {
+      user1Ties++
+      user2Ties++
+      // lets just update the user score and then do an update for all at the end
+
+    }
+    if (choice2 === "paper") {
+      user1Losses++
+      user2Wins++
+    }
+    if (choice2 === "scissor") {
+      user1Wins++
+      user2Losses++
+    }
+  }
+  if (choice1 === "paper") {
+    if(choice2 === "rock") {
+      user1Wins++
+      user2Losses++
+    }
+    if (choice2 === "paper") {
+      user1Ties++
+      user2Ties++
+    }
+    if (choice2 === "scissor") {
+      user1Losses++
+      user2Wins++
+    }
+  }
+  if (choice1 === "scissor") {
+    if (choice2 === "rock") {
+      user1Losses++
+      user2Wins++
+    }
+    if (choice2 === "paper") {
+      user1Wins++
+      user2Losses++
+    }
+    if (choice2 === "scissor") {
+      user1Ties++
+      user2Ties++
+    }
+  }
+        database.ref().child("player1").update({
+        wins: user1Wins,
+        losses: user1Losses,
+        ties: user1Ties
+      })
+      database.ref().child("player2").update({
+        wins: user2Wins,
+        losses: user2Losses,
+        ties: user2Ties
+      })
+      database.ref().child("choice1").update({
+        choice: null
+      })
+      database.ref().child("choice2").update({
+        choice: null
+      })
+    }
+})
 
 $("#submitUserName").on("click", function() {
  username = $("#text").val().trim()
  $(".temp").fadeOut()
  database.ref().child("chat").set({
   username: username
+ })
+ database.ref("player1").on("value", function(snapshot) {
+  user1 = snapshot.val().username
+  if (user1 == null) {
+    $("#player1").show()
+    console.log("a")
+  }
+   if (snapshot.val().username != null) {
+     console.log(user1)
+   }
+ })
+ database.ref("player2").on("value", function(snapshot) {
+  user2 = snapshot.val().username
+   if (user2 == null && user1 !== null) {
+    $("#player2").show();
+    console.log("a")
+  }
  })
 })
 $("#sendMessage").on("click", function() {
@@ -30,29 +161,81 @@ $("#sendMessage").on("click", function() {
 })
 
 database.ref().child("chat").on("child_added", function(snapshot) {
+  if(snapshot.val().time !== undefined) {
  let name = $("<div>").addClass("name").text("(" + snapshot.val().time + ") " + snapshot.val().user + ": " + snapshot.val().message)
  $("#chatbox").append(name)
+  }
 })
 
+// look and window events to see what happens when window is closed and use that to set player to null if not put log out button
+
+// window.onbeforeunload set the player you are = to null so if (user === snapshot.val().child("player1").user == username 
+
+// need to figure out user1 first then can do a function if
+// window.onbeforeunload = function 
+
+
+// if (user === snapshot.val().child("player2").user === username))
+// if undefined don't send message
+
 $("#player1").on("click", function() {
- $("#player1").fadeOut()
+ $("#player1").hide()
+ $("#player2").hide()
+ $("#user1").append(rock1, paper1, sissors1)
   database.ref().child("player1").set({
    username: username,
-   losses: 0,
-   wins: 0,
-   ties: 0
+   losses: user1Losses,
+   wins: user1Wins,
+   ties: user1Ties
+  })
+  // this is not setting it for other windows not going trough
+  database.ref().child("player1").on("value", function(snapshot) {
+    $("#user1name").text(snapshot.val().username)
+    $("#score1").html("<p>losses: " + snapshot.val().losses + "</p>" + "<p>wins: " + snapshot.val().wins +"</p>" + "<p>ties: " + snapshot.val().ties + "</p>")
   })
 })
+
+database.ref().child("player2").on("child_added", function(snapshot) {
+  // not setting this
+  user2 = snapshot.val().username
+  console.log(user1)
+})
+
+
 // need to make when log out gets deleted
 $("#player2").on("click", function() {
- $("#player2").fadeOut()
+ $("#player2").hide()
+ $("#user2").append(rock2, paper2, sissors2)
   database.ref().child("player2").set({
    username: username,
-   losses: 0,
-   wins: 0,
-   ties: 0
+   losses: user2Losses,
+   wins: user2Wins,
+   ties: user2Ties
   })
+  database.ref().child("player2").on("value", function(snapshot) {
+    $("#user2name").text(snapshot.val().username)
+    $("#score2").html("<p>losses: " + snapshot.val().losses + "</p>" + "<p>wins: " + snapshot.val().wins +"</p>" + "<p>ties: " + snapshot.val().ties + "</p>") 
+  })
+  
 })
+
+// will have a bug if multiple users of the same name so need to fix that
+// i can also make a log out button with similar function
+window.addEventListener("beforeunload", function() {
+  if (username = user1) {
+    database.ref().child("player1").update({
+      username: null
+    })
+  }
+  if (username = user2) {
+    database.ref().child("player1").update({
+      username: null
+    })
+  }
+})
+// this click not doing anything
+
+
 // right now appending
 
 // might do fadeout too so can't try to have multiple users on one browser window
@@ -66,4 +249,5 @@ $("#player2").on("click", function() {
 // when player enters name it updates the above objects and puts them in player1 or player2 if player1 already has a player
 // if there are more than two players say full
 // show the options only to the player whose turn it is
-// 
+// need to not show options until both players
+// need to not show user2 options until player one has picked and then put something in the middle saying whose turn it is also put the result like player1 tied or won etc
